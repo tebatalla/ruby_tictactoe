05fb273afb80d7bf58217d8ad0f336a2c84b4397
Tomas <tebatalla@gmail.com>
2014-10-29 23:05:09 -0500

    Check status of victory test and code

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,52 +1,88 @@
 Dir["lib/*.rb"].each {|file| require file }
 
-class Board
-	attr_reader :grid
-	def initialize
-		@grid = []
-		3.times do |i|
-			@grid << []
-			3.times do |j|
-				@grid[i][j] = "-"
+class Game
+
+	attr_reader :board, :player1, :player2
+
+	@@winning_combos = [
+						[0, 1, 2],
+						[3, 4, 5],
+						[6, 7, 8],
+						[0, 3, 6],
+						[1, 4, 7],
+						[2, 5, 8],
+						[0, 4, 8],
+						[2, 4, 6]
+						]
+
+	def initialize(player1_name, player2_name)
+		@board = Board.new
+		@player1 = Player.new(1, player1_name)
+		@player2 = Player.new(2, player2_name)
+	end
+
+	def turn(player)
+		@board.display_grid
+		puts "#{player.name}, please make a move (e.g. top left is 0, bottom right is 8)"
+		begin
+			position = gets.chomp.to_i
+			@board.make_move!(position, player.symbol)
+		rescue InvalidMoveError => e
+			puts e
+			turn(player)
+		end
+	end
+	# Checks to see if player has won the game
+	def won_game?(player)
+		player_spots = @board.grid.each_index.select { |spot| @board.grid[spot] == player.symbol }
+		@@winning_combos.any? do |combo|
+			player_spots & combo == combo
+		end
+	end
+
+	class Board
+		attr_reader :grid
+		def initialize
+			@grid = []
+			9.times do |i|
+				@grid[i] = "-"
 			end
 		end
-	end
 
-	def display_grid
-		output = ""
-		@grid.each do |row|
-			line = ""
-			row.each do |box|
-				line << " #{box} |"
+		def display_grid
+			output = ""
+			@grid.each_with_index do |box, position|
+				output << " #{box} "
+				output << "|" if [0, 1, 3, 4, 6, 7].include? position 
+				output << "\n-----------\n" if [2, 5].include? position 
 			end
-			output << line[0...-2]
-			output << "\n-----------\n" unless row.equal? @grid.last
+			return output
 		end
-		return output
-	end
 
-	def make_move(position, symbol)
-		if valid_move?(position)
-			@grid[position[0]][position[1]] = symbol
-		else
-			raise Exceptions::InvalidMoveError, "Invalid move"
+		def make_move!(position, symbol)
+			if valid_move?(position)
+				@grid[position] = symbol
+			else
+				raise Exceptions::InvalidMoveError, "Invalid move"
+			end
+			
 		end
-		
+
+		def valid_move?(position)
+			@grid[position] == "-"
+		end
 	end
 
-	def valid_move?(position)
-		@grid[position[0]][position[1]] == "-"
-	end
-end
+	class Player
+		attr_accessor :name, :turn
+		attr_reader :symbol
 
-class Player
-	attr_accessor :name, :turn
-	attr_reader :symbol
+		def initialize(turn, name)
+			@name = name
+			@turn = turn
+			@symbol = turn == 1 ? "X" : "O"
+		end
 
-	def initialize(turn, name)
-		@name = name
-		@turn = turn
-		@symbol = turn == 1 ? "X" : "O"
 	end
 
 end
--- test/tictactoe_spec.rb
+++ test/tictactoe_spec.rb
@@ -1,40 +1,40 @@
 Dir["../../ruby_tictactoe/lib/*.rb"].each {|file| require file }
 require 'rspec'
 
-
+new_game = Game.new("Julia", "Tomas")
+tictactoe = new_game.board
 describe "Board" do
-	tictactoe = Board.new
 	it "returns an initialized grid" do
-		expect(tictactoe.display_grid).to eq(" - | - | -\n-----------\n - | - | -\n-----------\n - | - | -")
+		expect(tictactoe.display_grid).to eq(" - | - | - \n-----------\n - | - | - \n-----------\n - | - | - ")
 	end
 
 	it "registers a move" do
-		tictactoe.make_move([0, 2], "X")
-		expect(tictactoe.display_grid).to eq(" - | - | X\n-----------\n - | - | -\n-----------\n - | - | -")
+		tictactoe.make_move!(2, "X")
+		expect(tictactoe.display_grid).to eq(" - | - | X \n-----------\n - | - | - \n-----------\n - | - | - ")
 	end
 
 	it "raises an exception when a player makes an invalid move" do
-		expect{tictactoe.make_move([0, 2], "X")}.to raise_error(Exceptions::InvalidMoveError, "Invalid move")
+		expect{tictactoe.make_move!(2, "X")}.to raise_error(Exceptions::InvalidMoveError, "Invalid move")
 	end
 end
 describe "Player" do
 	it "creates Player 1 with the name Julia and the symbol X" do
-		julia = Player.new(1, "Julia")
+		julia = new_game.player1
 		expect(julia.turn).to eq(1)
 		expect(julia.name).to eq("Julia")
 		expect(julia.symbol).to eq("X")
 	end
 	it "creates Player 2 with the name Tomas and the symbol O" do
-		tomas = Player.new(2, "Tomas")
+		tomas = new_game.player2
 		expect(tomas.turn).to eq(2)
 		expect(tomas.name).to eq("Tomas")
 		expect(tomas.symbol).to eq("O")
 	end
 end
 describe "Game" do
-	it "starts a new game" do
-		new_game = Game.new(julia, tomas)
-		expect(new_game.board.display_grid).to eq(" - | - | -\n-----------\n - | - | -\n-----------\n - | - | -")
+	it "correctly checks if a player has won the game" do
+		tictactoe.make_move!(0, "X")
+		tictactoe.make_move!(1, "X")
+		expect(new_game.won_game?(new_game.player1)).to eq(true)
 	end
-
 end
